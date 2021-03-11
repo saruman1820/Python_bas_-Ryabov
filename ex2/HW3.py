6e8f0d571dad4403bdd39233a0005312f2c005ed
Alexander.Ryabov <Ya.a-ryabov@ya.ru>
2021-03-11 22:57:04 +0300

    3поправил

--- ex2/HW3.py
+++ ex2/HW3.py
@@ -2,24 +2,23 @@
 # Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
 # Напишите решения через list и через dict.
 
-Lseasons = ['Зима', 'Весна', 'Лето', 'Осень']
+lseasons = ['Зима', 'Весна', 'Лето', 'Осень']
 Dseasons = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
 NUM_month = int(input("Введите месяц по номеру "))
+# Реализация через список
 if NUM_month in [1, 2, 12]:
-    print(Lseasons[0])
-    print(Dseasons.get(1))
-
+    print(lseasons[0])
 elif 2 < NUM_month < 6:
-    print(Lseasons[1])
-    print(Dseasons.get(2))
-
+    print(lseasons[1])
 elif 5 < NUM_month < 9:
-    print(Lseasons[2])
-    print(Dseasons.get(3))
-
+    print(lseasons[2])
 elif 8 < NUM_month < 12:
-    print(Lseasons[3])
-    print(Dseasons.get(4))
-
+    print(lseasons[3])
 else:
     print("Такого месяца не существует")
+
+# Реализация через словарь
+dseasons = {'Зима': [1, 2, 12], 'Весна': [3, 4, 5], 'Лето': [6, 7, 8], 'Осень': [9, 10, 11]}
+for k, v in dseasons.items():
+    if NUM_month in v:
+        print(k)
