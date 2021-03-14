e5cae33ebcdb3bfd9e91d5fda699c070b2a8cbb2
Alexander.Ryabov <Ya.a-ryabov@ya.ru>
2021-03-14 17:02:15 +0300

    4.5 готово

--- EX4/5.py
+++ EX4/5.py
@@ -5,4 +5,4 @@ from functools import reduce
 
 my_list = [el for el in range(100, 1001, 2)]
 print(my_list)
-print(reduce(lambda prev_el, el: prev_el + el, my_list))
+print(reduce(lambda prev_el, el: prev_el * el, my_list))
