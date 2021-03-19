ccf12a385f408ac1ceb82aa14a538455b9415dba
Alexander.Ryabov <Ya.a-ryabov@ya.ru>
2021-03-19 22:18:44 +0300

    2 готово

---
+++ EX5/2.py
@@ -0,0 +1,19 @@
+# Создать текстовый файл (не программно), сохранить в нем несколько строк,
+# выполнить подсчет количества строк, количества слов в каждой строке.
+
+
+try:
+    f_obj = open("1.txt", "r")
+    content = f_obj.readlines()
+    print(content)
+    lines = len(content)
+    for index, el in enumerate(content ,1 ):
+        print(f"В {index}-й строке {len(el.split())} слов(a)" )
+    print(f"в файле {lines} строк(и)")
+
+
+
+except IOError:
+    print("Произошла ошибка ввода-вывода!")
+finally:
+    f_obj.close()
