b4a8f01faa75536e1add19dd810da7e66e1f141c
Alexander.Ryabov <Ya.a-ryabov@ya.ru>
2021-03-21 21:41:34 +0300

    6.4 готово но не точно

---
+++ EX6/6.py
@@ -0,0 +1,40 @@
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'{self.title}'
+
+
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+brush = Stationary("кисть")
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print(brush.draw())
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
