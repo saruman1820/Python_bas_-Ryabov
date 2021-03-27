aac694c78829bfd9ae84ddeebee203a0d1dba81f
Alexander.Ryabov <Ya.a-ryabov@ya.ru>
2021-03-28 00:46:00 +0300

    8.4 готово

--- EX8/4.py
+++ EX8/4.py
@@ -25,13 +25,17 @@ class Office_equipment:
 
 
 class Printer(Office_equipment):
-    def __init__(self, price_one_sheet: float, max_count_sheet_per_one_cartridge: int):
+    def __init__(self, price_one_sheet: float, max_count_sheet_per_one_cartridge: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.price_one_sheet = price_one_sheet
         self.max_count_sheet_per_one_cartridge = max_count_sheet_per_one_cartridge
 
 
 class Scanner(Office_equipment):
-    def __init__(self,  auto_feed: bool = False):
+    def __init__(self, auto_feed: bool = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.auto_feed = auto_feed
 
 
@@ -39,7 +43,41 @@ class Mfu(Office_equipment):
     def __init__(self,
                  price_one_sheet: float,
                  max_count_sheet_per_one_cartridge: int,
-                 auto_feed: bool = False):
+                 auto_feed: bool = False,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.price_one_sheet = price_one_sheet
         self.max_count_sheet_per_one_cartridge = max_count_sheet_per_one_cartridge
         self.auto_feed = auto_feed
+
+
+Printer1 = Printer(manufacturer="Xerox", model="202", count_sheets_per_minute=30, interface="ethernet",
+                   price_one_sheet=0.5, max_count_sheet_per_one_cartridge=1000)
+
+Printer2 = Printer(manufacturer="Xerox", model="202", count_sheets_per_minute=30, interface="ethernet",
+                   price_one_sheet=0.5, max_count_sheet_per_one_cartridge=1000)
+
+Printer3 = Printer(manufacturer="Xerox", model="202", count_sheets_per_minute=30, interface="ethernet",
+                   price_one_sheet=0.5, max_count_sheet_per_one_cartridge=1000)
+
+scanner1 = Scanner(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="usb",
+                   auto_feed=True)
+scanner2 = Scanner(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="usb",
+                   auto_feed=False)
+scanner3 = Scanner(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="ethernet",
+                   auto_feed=True)
+
+mfu1 = Mfu(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="ethernet",
+           price_one_sheet=0.5, max_count_sheet_per_one_cartridge=1000, auto_feed= True)
+
+mfu2 = Mfu(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="ethernet",
+           price_one_sheet=0.5, max_count_sheet_per_one_cartridge=1000, auto_feed= False)
+
+mfu3 = Mfu(manufacturer="hp", model="330", count_sheets_per_minute=60, interface="ethernet",
+           price_one_sheet=0.5, max_count_sheet_per_one_cartridge=50000, auto_feed= True)
+
+
+
+
